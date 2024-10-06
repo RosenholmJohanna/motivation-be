3fd241078c81112699d42b06dd8619d8025e2e1b
rosenholmjohanna <rosenholm.johanna88@gmail.com>
2024-10-06 02:06:28 +0200

    jwt as auth functionality added

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
-import express from "express"
-import cors from "cors"
-import mongoose from "mongoose"
-import bcrypt from "bcrypt"
-//const Schema = mongoose.Schema;
-//import UserSchema from "./models/user";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import crypto from "crypto";
+import jwt from "jsonwebtoken";
+//import jsonwebtoken from "jsonwebtoken";
 import { User } from "./models/user.js";
 
+dotenv.config();
+
+const secretKey = crypto.randomBytes(64).toString("hex");
+// console.log(secretKey);
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/motivation";
+//const accessToken = jwt.sign({ userId: User._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-//const User = mongoose.model("User", UserSchema); 
-
-mongoose.connect(mongoUrl).then(() => {
-  console.log("Connected to the Database successfully");
-}).catch(err => {
-  console.error("Database connection error:", err);
-});
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log("Connected to the Database successfully");
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+  });
 mongoose.Promise = Promise;
 
 const port = process.env.PORT || 8080;
@@ -28,70 +36,109 @@ app.get("/", (req, res) => {
   res.send("welcome to motivational");
 });
 
-
-
-  // USER REGISTRATION
-  app.post("/register", async (req, res) => {
-    const { username, password } = req.body;
-    try {
-      const salt = bcrypt.genSaltSync();
-      if (password.length < 3) {
-        res.status(400).json({
-          success: false,
-          response: "Password must be at least 3 characters long",
-        });
-      } else {
-        const newUser = await new User({
-          username: username,
-          password: bcrypt.hashSync(password, salt),
-        }).save();
-        res.status(201).json({
-          success: true,
-          response: {
-            username: newUser.username,
-            //accessToken: newUser.accessToken,
-          },
-        });
-      }
-    } catch (error) {
+// USER REGISTRATION
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const salt = bcrypt.genSaltSync();
+    if (password.length < 3) {
       res.status(400).json({
         success: false,
+        response: "Password must be at least 3 characters long",
+      });
+    } else {
+      const newUser = await new User({
+        username: username,
+        password: bcrypt.hashSync(password, salt),
+      }).save();
+      res.status(201).json({
+        success: true,
+        response: {
+          username: newUser.username,
+          //accessToken: newUser.accessToken,
+        },
       });
     }
-  });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+    });
+  }
+});
 
-
-
-// USER LOGIN 
+// USER LOGIN
 app.post("/login", async (req, res) => {
-  const { username, password} = req.body;
+  const { username, password } = req.body;
   try {
-    const user = await User.findOne({username});
+    const user = await User.findOne({ username });
     if (user && bcrypt.compareSync(password, user.password)) {
+      const accessToken = jwt.sign(
+        { userId: user._id },
+        process.env.JWT_SECRET,
+        { expiresIn: "1h" }
+      );
+
       res.status(200).json({
         success: true,
         response: {
-          // username: user.username,
-          // id: user._id,
-          user: user
-        }
+          token: accessToken,
+          user: {
+            id: user._id,
+            username: username.username,
+          },
+        },
       });
     } else {
       res.status(400).json({
         success: false,
-        response: "Credentials didn't match"
+        response: "Credentials didn't match",
       });
     }
   } catch (error) {
     res.status(500).json({
       success: false,
-      response: error
+      response: error,
     });
   }
 });
 
+// middleware verifying JWT
+const authenticateUser = (req, res, next) => {
+  const authHeader = req.header("Authorization");
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({
+      success: false,
+      message: "Access denied. No token provided.",
+    });
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "Invalid token.",
+    });
+  }
+};
 
+// protected route 
+app.get("/protected", authenticateUser, (req, res) => {
+  res
+    .status(200)
+    .json({
+      success: true,
+      message: "You have access to this protected route!",
+      userId: req.user.userId,
+    });
+});
 
-  app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-  });
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
+//const User = mongoose.model("User", UserSchema);
+//const Schema = mongoose.Schema;
+//import UserSchema from "./models/user";
